1b7622668a292bcb846dda503a207ceb7130e9d4
OUASMI YOUNES <y.ouasmi@gmail.com>
2024-05-09 14:46:55 +0200

    Initial Commit

---
+++ Le_Baluchon/Controller/ForecastViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForecastViewController.swift
+//  Le_Baluchon
+//
+//  Created by younes ouasmi on 09/05/2024.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Le_Baluchon/Controller/TranslateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TranslateViewController.swift
+//  Le_Baluchon
+//
+//  Created by younes ouasmi on 09/05/2024.
+//
+
+import UIKit
+
+class TranslateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
